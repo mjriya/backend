5ec6db1b2abf86021faff287ac1c9882e45cee81
manashbarai <manashjbarai@gmail.com>
2025-02-01 23:48:04 +0530

    avoid firebase

--- src/loaders/firebaseAdmin.loader.js
+++ src/loaders/firebaseAdmin.loader.js
@@ -1,23 +1,23 @@
-import admin from "firebase-admin";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-import fs from "fs";
+// import admin from "firebase-admin";
+// import { fileURLToPath } from "url";
+// import { dirname, resolve } from "path";
+// import fs from "fs";
 
 
 
-// Resolve current directory
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// // Resolve current directory
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-// Dynamically load the JSON file
-const serviceAccountPath = resolve(__dirname, "../../edureify-firebase-adminsdk-m3r6f-73c2ea5deb.json");
-const serviceAccount = JSON.parse(await fs.promises.readFile(serviceAccountPath, "utf8"));
+// // Dynamically load the JSON file
+// const serviceAccountPath = resolve(__dirname, "../../edureify-firebase-adminsdk-m3r6f-73c2ea5deb.json");
+// const serviceAccount = JSON.parse(await fs.promises.readFile(serviceAccountPath, "utf8"));
 
-// console.log("serviceAccount: ", serviceAccount);
+// // console.log("serviceAccount: ", serviceAccount);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
-export { admin };
+// export { admin };
 
