ea2628904f4c7696c725f6dc0e77d3c1dcb02bcb
manashbarai <manashjbarai@gmail.com>
2025-02-01 23:52:30 +0530

    update git ignore

--- src/loaders/firebaseAdmin.loader.js
+++ src/loaders/firebaseAdmin.loader.js
@@ -1,23 +1,24 @@
-// import admin from "firebase-admin";
-// import { fileURLToPath } from "url";
-// import { dirname, resolve } from "path";
-// import fs from "fs";
+import admin from "firebase-admin";
+import dotenv from "dotenv";
 
+dotenv.config();
 
+// Load Firebase credentials from environment variables
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+};
 
-// // Resolve current directory
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
-// // Dynamically load the JSON file
-// const serviceAccountPath = resolve(__dirname, "../../edureify-firebase-adminsdk-m3r6f-73c2ea5deb.json");
-// const serviceAccount = JSON.parse(await fs.promises.readFile(serviceAccountPath, "utf8"));
-
-// // console.log("serviceAccount: ", serviceAccount);
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
-
-// export { admin };
-
+export { admin };
